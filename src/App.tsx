f153fa7a31c4607ced4993acf90437da96f471db
Labinot Veseli <labinotveseli1@gmail.com>
2023-06-16 16:19:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
